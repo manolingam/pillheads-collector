5b1cd5ae5a8832b2c0912164f0ad1eb17cee05f0
Manolingam N <manolingam1996@gmail.com>
2022-08-18 19:48:09 +0530

    refresh bug fixed

--- views/Profile.jsx
+++ views/Profile.jsx
@@ -1,4 +1,4 @@
-import { Flex, SimpleGrid, Box, Text, Link } from '@chakra-ui/react';
+import { Flex, SimpleGrid, Box, Text, Link, Spinner } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
 import Image from 'next/image';
@@ -9,13 +9,17 @@ import { getAccountString } from '../utils/helpers';
 
 export const Profile = ({ pillheads }) => {
   const [tokenMeta, setTokenMeta] = useState([]);
+  const [metadataFetched, setMetadataFetched] = useState(false);
 
   const grabTokenMeta = async () => {
     let tokeMeta = [];
     pillheads.forEach(async (pillhead, index) => {
       const metadata = await fetchPillheadMetadata(pillhead.tokenUri);
       tokeMeta.push(metadata);
-      index === pillheads.length - 1 ? setTokenMeta(tokeMeta) : null;
+      if (index === pillheads.length - 1) {
+        setTokenMeta(tokeMeta);
+        setMetadataFetched(true);
+      }
     });
   };
 
@@ -32,41 +36,45 @@ export const Profile = ({ pillheads }) => {
       mt='4rem'
       mb='4rem'
     >
-      <SimpleGrid columns={{ lg: 6, md: 2, sm: 1 }} spacing={10}>
-        {tokenMeta.map((token, index) => (
-          <Link
-            key={index}
-            href={`https://opensea.io/assets/ethereum/0x1b23d0f0f6dc3547c1b6945152acbfd6eaad85b0/${token.name.substring(
-              10
-            )}`}
-            isExternal
-            _hover={{
-              textDecoration: 'none',
-              transform: 'scale(1.1)'
-            }}
-          >
-            <Box position='relative'>
-              <Image
-                src={token.image}
-                alt='pillhead'
-                width={250}
-                height={250}
-                placeholder='blur'
-                blurDataURL={token.image}
-              />
-              <Text
-                bg={theme.colors.brand.yellowOne}
-                fontFamily={theme.fonts.disketMono}
-                fontSize='12px'
-                p='1rem'
-                textAlign='center'
-              >
-                {token.name}
-              </Text>
-            </Box>
-          </Link>
-        ))}
-      </SimpleGrid>
+      {metadataFetched ? (
+        <SimpleGrid columns={{ lg: 6, md: 2, sm: 1 }} spacing={10}>
+          {tokenMeta.map((token, index) => (
+            <Link
+              key={index}
+              href={`https://opensea.io/assets/ethereum/0x1b23d0f0f6dc3547c1b6945152acbfd6eaad85b0/${token.name.substring(
+                10
+              )}`}
+              isExternal
+              _hover={{
+                textDecoration: 'none',
+                transform: 'scale(1.1)'
+              }}
+            >
+              <Box position='relative'>
+                <Image
+                  src={token.image}
+                  alt='pillhead'
+                  width={250}
+                  height={250}
+                  placeholder='blur'
+                  blurDataURL={token.image}
+                />
+                <Text
+                  bg={theme.colors.brand.yellowOne}
+                  fontFamily={theme.fonts.disketMono}
+                  fontSize='12px'
+                  p='1rem'
+                  textAlign='center'
+                >
+                  {token.name}
+                </Text>
+              </Box>
+            </Link>
+          ))}
+        </SimpleGrid>
+      ) : (
+        <Spinner size='lg' color='black' />
+      )}
     </Flex>
   );
 };
