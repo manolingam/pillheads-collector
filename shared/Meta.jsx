271815f2f567821417d56da319b5cd1142ac2371
Manolingam N <manolingam1996@gmail.com>
2022-08-18 17:34:45 +0530

    meta data update

--- shared/Meta.jsx
+++ shared/Meta.jsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 
+const logo =
+  'https://res.cloudinary.com/saimano/image/upload/v1660824147/Pillheads/banner_yuqafa.jpg';
+
 export const Meta = () => {
   return (
     <>
@@ -7,13 +10,23 @@ export const Meta = () => {
         <title>Pillheads Profiler</title>
         <meta
           name='description'
-          content='A sharable profile of the Pillheads NFTs.'
+          content='A sharable profile for your Pillheads NFTs.'
         />
         <meta property='og:title' content='Pillheads Profiler' />
         <meta
           property='og:description'
-          content='A sharable profile of the Pillheads NFTs.'
+          content='A sharable profile for your Pillheads NFTs.'
         />
+
+        <meta property='og:image' content={logo} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='Pillheads Profiler' />
+        <meta
+          name='twitter:description'
+          content='A sharable profile for your Pillheads NFTs.'
+        />
+        <meta name='twitter:image' content={logo} />
+        <meta property='og:type' content='website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
     </>
